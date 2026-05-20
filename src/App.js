440a03d1e33224f61fb92d94f062be4010857dfa
harshadadivekar-spec <harshadadivekar799@gmail.com>
2026-05-20 11:40:30 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to React</h2>
+      <h2 style={{"color":"red"}}>Welcome to React</h2>
+      <h2>Welcome  Back</h2>
     </div>
   );
 }
